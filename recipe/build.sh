32b192c4fc7fec401d2f0e6d14fad19062e541ff
Richard Höchenberger <richard.hoechenberger@gmail.com>
2022-05-25 21:14:16 +0200

    Disable static linking

--- recipe/build.sh
+++ recipe/build.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 
 mkdir build && cd build
-cmake ${CMAKE_ARGS} -DCMAKE_INSTALL_PREFIX:PATH=$PREFIX ..
+cmake ${CMAKE_ARGS} -DUSE_STATIC_RUNTIME:BOOL=OFF -DCMAKE_INSTALL_PREFIX:PATH=$PREFIX ..
 make install
